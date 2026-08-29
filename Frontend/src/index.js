076b9a9420ff1de89f25551e2bb0f647f96b3354
Vishal Kumar <vsingh5894@gmail.com>
2026-08-29 21:33:58 +0530

    Updated index.js file

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App_UPDATED_LightMode';
-import './App_UPDATED_LightMode.css';
+import App from './App';
+import './App.css';
 
 // Create root and render App
 const root = ReactDOM.createRoot(document.getElementById('root'));
